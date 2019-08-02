f7826543b76dcc996cea44d541dd7f7bec8a0d0d
Javier <Javier@172.29.132.255>
2019-08-02 10:59:14 -0500

    JAVIER: Cambios del 02/08/2019 11:00 Talleres

--- src/main/java/epn/edu/ec/servicios/LoginServicio.java
+++ src/main/java/epn/edu/ec/servicios/LoginServicio.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package epn.edu.ec.servicios;
 
 import epn.edu.ec.modelo.Usuario;
 import epn.edu.ec.utilidades.Constantes;
 import javax.ws.rs.core.Response;
 
-/**
- *
- * @author User
- */
 public class LoginServicio {
     
     private final ConexionServicio<Usuario> conexion;    
@@ -28,7 +19,6 @@ public class LoginServicio {
         Response response= conexion.conexion(URL_USUARIO_LOGIN+"/login", "POST", false, usuario);
         if(response.getStatus()==200){
             usuarioAux =response.readEntity(Usuario.class);
-            
         }
         
         return usuarioAux;
